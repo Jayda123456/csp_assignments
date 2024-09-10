829a23399d79e538a01e3b135026cfeae1f1bb44
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-10 19:25:51 +0000

    I did my variables practice

---
+++ Variables/practice.py
@@ -0,0 +1,20 @@
+name = "Jayda"
+num = 5
+print(name)
+print(num)
+num = 500
+breakfast = "cereal"
+favcolor = "light blue"
+schoolname = "UCAS"
+year = 2024
+Icolor = "blue"
+age = 15
+favsubject = "math"
+print(num)
+print(breakfast)
+print(favcolor)
+print(schoolname)
+print(year)
+print(Icolor)
+print(age)
+print(favsubject)
