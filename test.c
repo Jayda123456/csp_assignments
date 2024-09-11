44183bbb3920e495da565a0c6b5f3d0823c62ce6
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-11 19:23:33 +0000

    I changed my code

--- test.c
+++ test.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-     
-    char name[]= "Jayda";
-    printf ("%s" , name);
-    return 0;
-    
+int main(void){
+char name[] = "Jayda";
+int smallNum = 4;
+printf("%s\n", name);
+printf("%d\n", smallNum);
 }
