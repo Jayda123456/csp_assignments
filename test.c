b741c89f0799099a82608037a1e79bc17f303ddf
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-11 22:48:53 +0000

    fixed my code

--- test.c
+++ test.c
@@ -8,17 +8,17 @@ printf("%s\n", name);
 int smallNum = 4;
 printf("%d\n", smallNum);
 
-int bignum = 50;
-printf("%d\n", bignum);
+int bigNum = 50;
+printf("%d\n", bigNum);
 
 char breakfast[] = "strawberries";
 printf("%s\n", breakfast);
 
 char favcolor[] = "light blue";
-printf("%s\n", light blue);
+printf("%s\n", favcolor);
 
 char school[] = "UCAS";
-printf("%s\n, UCAS");
+printf("%s\n", school);
 
 
 }
