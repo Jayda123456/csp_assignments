091a1f98b03e467d5528938680210a928db432e4
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-18 19:43:07 +0000

    I did my practice code

---
+++ Expressions/practice.py
@@ -0,0 +1,17 @@
+question1 = 7-24/8*4+6
+print(question1)
+question2 = 18/3-7+25
+print(question2)
+question3 = 6*4/12+72/8-9
+print(question3)
+question4 = (17-6/2)+43
+print(question4)
+question5 = -2*(1*4-2/2)+(6+2-3)
+print(question5)
+question6 = -1*((3-4*7)/5)-2*24/6 
+print(question6)
+question7 = (3*5**2/15)-(5-2**2) 
+print(question7)
+question8 = (1**4*2**2+3**3)-2**5/4
+print(question8)
+question9 = (22/2-2.5)**2+(4-6/6)**2
