fddabe6a48b794280b54dfbdaff3c0b6f0ce89a3
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-20 19:40:07 +0000

    i fixed my code

---
+++ Expressions/strings/sillySentances.py
@@ -0,0 +1,5 @@
+noun = input("tell me a noun")
+verb = input("tell me a verb")
+animal = input("tell me a animal")
+print("The person was " + verb + " along side a " + animal + " with a " + noun + ".")
+
