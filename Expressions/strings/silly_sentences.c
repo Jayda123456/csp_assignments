a7efbec6d12891fdd5c556f3473e9f960e62c5e6
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-20 00:50:51 +0000

    did my code

---
+++ Expressions/strings/silly_sentences.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    char animal{20};
+    char place{20};
+    char verb{20};
+    char sentence{400}; = 
+    printf ("name an animal: ");
+    scanf("name of animal:");
+    printf("name a place: ");
+    scanf("name of place:");
+    printf(:name a past tense verb: ");
+    scanf("name of verb:");
+    strcat(sentence, animal);
+    strcat(sentence, "went to the");
+    strcat(sentence, place);
+    strcat(sentence, "and");
+    strcat(sentence, verb);
+    strcat(sentence, "with his friend the angry turtle,\n");
+    printf("%s, sentence);
+    
+    return 0;","
+}
