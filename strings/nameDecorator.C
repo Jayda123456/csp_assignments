3acbe6951c9f39213b0635e32dea3447c436a029
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-26 19:37:08 +0000

    I did my decorator C

---
+++ strings/nameDecorator.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+int main(void){
+    char name[30];
+    printf( "what is your name:"); 
+    scanf("%s",name );
+    printf("<<<%s>>>",name );
+    return 0 ;
+} 
+    
+    
