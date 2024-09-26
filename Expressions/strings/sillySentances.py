f832c7d4aaca4a66e4cca68f2a3c28e961087019
Jayda123456 <jayda.adams@ucas-edu.net>
2024-09-26 19:06:09 +0000

    i did my code

--- Expressions/strings/sillySentances.py
+++
@@ -1,5 +0,0 @@
-noun = input("tell me a noun")
-verb = input("tell me a verb")
-animal = input("tell me a animal")
-print("The person was " + verb + " along side a " + animal + " with a " + noun + ".")
-
--- functions/update_budget.c
+++ functions/update_budget.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
-float income, rent, utilities, groceries, transportation, savings,exspenses, spend;
+float income, rent, utilities, groceries, transportation, savings, exspenses, spend;
 
-
-void percent(char type[], float var){
-int per = amount/income * 100;
-scanf( "%.2f ,&var");
-return var;
-
-
-float input(char type[]){
-printf("monthly%s:\n", type);
+void input(char type[], float var){
+    printf("monthly %s:\n", type);
+    scanf( "%f" , &var");
+    return var;
 }
 
+void percent(char type[], int amount){
+    int per = amount/income *100;
 
-printf("your %s is %d%% of your income.\n", type, per );
+
+printf("your %s is %d%% of your income.\n", type, per);
 }
 
 
 int main(void){
-float income, rent, utilities, groceries, transportation, savings,exspenses, spending;
-float prent, putilities, pgroceries, ptransportation, psaving, pexspenses, pspending;
-printf("this is a budget calulator.\n how much do you make a month?\n");
-input("income");
-scanf("%f", &income);
-printf("how much do your utilities cost\n");
-scanf("%f", &utilities);
-printf("how much do your groceries cost\n");
-scanf("%f", &groceries);
-printf("how much do your transportation cost\n");
-scanf("%f", &transportation);
-printf("How much does your rent cost?");
-scanf("%f", &rent);
-exspenses = rent + groceries + transportation + utilities;
-spend = income - exspenses - savings; printf(" you make %.2f\n",income);
-printf("your exspenses are %.2f\n", exspenses);
-printf("your savings are $%.2f\n", savings);
-printf ("your spending is &%.2f\n", spend);
+printf("this is a budget calculator. \n");
+income = input("income", income);
+rent = input("rent", rent);
+utilities = input("utilities", utilities);
+transportation = input("transportation", transportation);
+savings = income * .2;
+exspenses = rent + utilities + groceries + transportation;
+spend = income - exspenses - savings;
+printf("you make $%.2f\n" , income);
+printf("your exspenses are $%.2f\n" , exspenses);
+printf("your savings are $%.2f\n" , savings);
+printf("your spending money is $%.2f\n", spend);
 
 
 percent("rent", rent );
@@ -44,7 +36,7 @@ percent("groceries", groceries);
 percent("transportation", transportation);
 percent("exspenses", exspenses);
 percent("savings", savings);
-percent("spending", spending);
+percent("spending money", spend);
 
 return 0;
 }
