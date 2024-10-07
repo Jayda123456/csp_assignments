c627ab5661ab2ba5a57534f1e57aaf7f50e37992
Jayda123456 <jayda.adams@ucas-edu.net>
2024-10-07 18:59:31 +0000

    doing my code

--- strings/sillysentence.c
+++ strings/sillysentence.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main(void){
+int main(void) {
     char animal[20];
-    printf("Name a animal\n");
+    printf("Name an animal: ");
     scanf("%s", animal);
+    printf("%s", animal);
+    char place[20];
+    printf("name a place: ");
+    scanf("%s", place);
+    printf("%s\n", place);
+    char verb[20];
+    printf("name a verb: ");
+    scanf("%s", verb);
+    printf("%s\n", verb);
+    printf("%s\n", animal, place, verb);
     return 0 ;
 }
--- strings/sillysentences.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main(void) {
-    char animal[20];
-    printf("name a  dog ");
-    scanf("%s", animal);
-    return 0;
-}
