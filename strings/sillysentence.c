aac3e0a30c11be159d7ecbdce227b292aaec8887
Jayda123456 <jayda.adams@ucas-edu.net>
2024-10-09 01:41:35 +0000

    finished my code

--- strings/sillysentence.c
+++ strings/sillysentence.c
@@ -4,15 +4,16 @@ int main(void) {
     char animal[20];
     printf("Name an animal: ");
     scanf("%s", animal);
-    printf("%s", animal);
+    
     char place[20];
-    printf("name a place: ");
+    printf("Name a place: ");
     scanf("%s", place);
-    printf("%s\n", place);
+    
     char verb[20];
-    printf("name a verb: ");
+    printf("Name a verb: ");
     scanf("%s", verb);
-    printf("%s\n", verb);
-    printf("%s\n", animal, place, verb);
-    return 0 ;
+    
+    printf("You said: %s in %s while %s.\n", animal, place, verb);
+    
+    return 0;
 }
