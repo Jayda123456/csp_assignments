be3bc9ef425cc79548cf8316d820e8739291799b
Jayda123456 <jayda.adams@ucas-edu.net>
2024-10-19 03:24:29 +0000

    fixed my code

--- conditions/oldEnough.py
+++ conditions/oldEnough.py
@@ -1,14 +1,13 @@
 age = 18
+
+
 if age >= 18:
-    print("you are old enough to vote")
-age = 16
-if age >= 15:
-    print("you are old enouph to get a leaners permit")
-age = 15
-if age >= 15:
-    print("you are old enough to drive")
-age = 4
-if age == 5:
+    print("you are old enough to vote, get a drivers permit, drive, and go to school")
+elif age >= 15:
+    print("you are old enough to vote, get a drivers permit, drive, and go to school")
+elif age >= 15:
+    print("you are old enough to vote, get a drivers permit, drive, and go to school")
+elif age >= 4:
         print("you are old enough to go school")
 else:
         print("you are are too young to go school")
