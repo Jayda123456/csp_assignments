c1240c8f8e108ddcd5ab0ceca871c0d2f706666e
Jayda123456 <jayda.adams@ucas-edu.net>
2024-10-23 02:12:11 +0000

    did it

--- loops/family loop.py
+++ loops/family loop.py
@@ -1,6 +1,4 @@
-list = ["wyatt", "brooklyn", "reese", "Jayda", "JD"]
-print("hello", "wyatt")
-print("hello", "brooklyn")
-print("hello", "reese")
-print("hello", "Jayda")
-print("hello", "JD")
+siblings = ["wyatt", "brooklyn", "reese", "Jayda", "JD"]
+for sibling in siblings:
+    print(f"Hello, {sibling}!")
+
