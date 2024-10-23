5ceb019c49954376334e37e29763c0da2438e595
Jayda123456 <jayda.adams@ucas-edu.net>
2024-10-23 19:23:37 +0000

    did my code

---
+++ loops/familyloop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+char name[50]= ("wyatt" "brooklyn"" Reese" "Jayda" "jd");
+int x;
+char main(){
+        for (x>0;x<5;){
+        printf("%d", name);
+        }
+    return 0;
+    }
